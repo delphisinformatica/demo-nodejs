4e77d7b1e548a838166fa54460176afd59e9f444
delphisinformatica <148489395+delphisinformatica@users.noreply.github.com>
2023-10-22 22:55:29 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var system = process.arch
 
+var pippo = process.env.pippo;
+
 var os = require('os');
 
 if(system === "x64") {
@@ -12,7 +14,7 @@ if(system === "x64") {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Demo Openshift', menuId:'home', arch:system, so:os.platform(), hostname:os.hostname(), kernel:os.release()});
+  res.render('index', {page:'Demo Openshift', menuId:'home', arch:system, so:os.platform(), hostname:os.hostname(), kernel:os.release(), pippo:pippo});
 });
 
 module.exports = router;
